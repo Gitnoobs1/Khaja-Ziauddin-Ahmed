a4c152e008b8c299bd37e6ded5363b1ad42ce201
Zia <119518955+Gitnoobs1@users.noreply.github.com>
2026-09-08 11:50:11 +0530

    Add files via upload

---
+++ Code 1.py
@@ -0,0 +1,8 @@
+a = int(input("Enter a value:"))
+b = int(input("Enter a value:"))
+
+print("Sum:",a+b)
+print("Difference:",a-b)
+print("Product:",a*b)
+print("Remainder:",a//b)
+print("Quotient:",a%b)
